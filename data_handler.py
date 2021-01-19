6fd8a185560789e24640cf92d02d4ebf74d4d1cd
Marci <smarcell91@gmail.com>
2021-01-19 10:37:25 +0100

    init

---
+++ data_handler.py
@@ -0,0 +1,105 @@
+import csv
+import os
+import re
+
+#id,submission_time,view_number,vote_number,title,message,image
+fieldnames_q = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
+fieldnames_a = ["id", "submission_time", "vote_number", "question_id", "message", "image"]
+ALLOWED_EXTENSIONS = {'png', 'jpg'}
+
+def writer(data, file, x):
+    if file == "sample_data/question.csv":
+        fieldnames = fieldnames_q
+    else:
+        fieldnames = fieldnames_a
+    writer = csv.DictWriter(open(file, x), fieldnames=fieldnames)
+    if x == "w":
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
+    else:
+        writer.writerow(data)
+
+
+def reader(csv_file):
+    reader = csv.DictReader(open(csv_file))
+    return reader
+
+
+def edit(data, file):
+    read = reader(file)
+    new = []
+    for row in read:
+        if data["id"] != row["id"]:
+            new.append(row)
+        else:
+            row.update(data)    #update the edited row/question
+            new.append(row)
+    writer(new, file, "w")
+
+
+def delete_answer(answer_id):
+    a_reader = reader("sample_data/answer.csv")
+    new_a = []
+    for row in a_reader:
+        if answer_id == row["id"]:
+            if row["image"]:
+                if os.path.exists(f"static/img/answer/{row['image']}"):
+                    os.remove(f"static/img/answer/{row['image']}")
+        else:
+            new_a.append(row)
+    writer(new_a, "sample_data/answer.csv", "w")
+
+
+def delete(id):
+    q_reader = reader("sample_data/question.csv")
+    a_reader = reader("sample_data/answer.csv")
+    new_q = []
+    new_a = []
+    for row in q_reader:
+        if id == row["id"]:
+            if row["image"]:
+                if os.path.exists(f"static/img/question/{row['image']}"):
+                    os.remove(f"static/img/question/{row['image']}")    #delete the file(image)
+        else:
+            new_q.append(row)
+    for row in a_reader:
+        if id == row["question_id"]:
+            if row["image"]:
+                if os.path.exists(f"static/img/answer/{row['image']}"):
+                    os.remove(f"static/img/answer/{row['image']}")
+        else:
+            new_a.append(row)
+    writer(new_q, "sample_data/question.csv", "w")
+    writer(new_a, "sample_data/answer.csv", "w")
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def id_generator(file):
+    id = 1
+    read = reader(file)
+    for row in read:
+        if row:
+            id = int(row["id"]) + 1
+    return id
+
+
+def sort(x):
+    d = re.split("=|&", x)[3]
+    y = re.split("=|&", x)[1]
+    print("sorter:",x, d)
+    questions = reader("sample_data/question.csv")
+    questions = list(questions)
+    try:
+        lst = sorted(questions, key = lambda i: int(i[y]))
+    except:
+        lst = sorted(questions, key=lambda i: i[y].lower())
+    if d == "down":
+        lst = lst[::-1]
+    for row in lst:
+        print(row)
+    return lst
--- server.py
+++ server.py
@@ -1,12 +1,170 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
+import os
+import data_handler
+
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'static/img'
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = {'png', 'jpg'}
 
 
+@app.route("/list")
 @app.route("/")
-def hello():
-    return "Hello World!"
+def list():
+        question = data_handler.reader("sample_data/question.csv")
+        quest = []
+        a = [x for x in question]
+        for row in a[::-1]:
+            quest.append(row)
+        return render_template("list.html", questions=quest)
+
+
+@app.route("/visitor/<question_id>")
+def visitor(question_id):
+    question = data_handler.reader("sample_data/question.csv")
+    for row in question:
+        if question_id == row["id"]:
+            row["view_number"] = int(row["view_number"]) + 1
+            data_handler.edit(row, "sample_data/question.csv")
+    return redirect(f"/question/{question_id}")
+
+
+@app.route("/question/<question_id>", methods=["GET", "POST"])
+def quest(question_id):
+    question = data_handler.reader("sample_data/question.csv")
+    answer = data_handler.reader("sample_data/answer.csv")
+    return render_template("question.html", question = question, answer = answer, id = question_id)
+
+
+@app.route("/add-question", methods=["GET", "POST"])
+def add_question():
+    return render_template("add_question.html")
+
+
+@app.route("/question/<question_id>/new-answer", methods=["GET", "POST"])
+def new_answer(question_id):
+    questions = data_handler.reader("sample_data/question.csv")
+    for row in questions:
+        if row["id"] == question_id:
+            return render_template("new_answer.html", question=row, id=question_id)
+
+
+@app.route("/save_answer/<question_id>", methods=["GET", "POST"])
+def save_answer(question_id):
+    if request.method == "POST":
+        id = data_handler.id_generator("sample_data/answer.csv")
+        answer = request.form["answer"]
+        f = request.files["file"]
+        if f:
+            f.filename = "id"+str(id)+f.filename
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], "answer", f.filename))
+        new_answer = {"id": id, "submission_time": datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "vote_number": 0,
+                      "question_id": question_id, "message": answer, "image": f.filename}
+        data_handler.writer(new_answer, "sample_data/answer.csv", "a")
+    return redirect(f"/question/{question_id}")
+
+
+@app.route("/save", methods=["GET", "POST"])
+def save():
+    if request.method == "POST":
+        id = data_handler.id_generator("sample_data/question.csv")
+        title = request.form["title"]
+        message = request.form["message"]
+        f = request.files["file"]
+        if f:
+            f.filename = "id"+str(id)+f.filename
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'],"question", f.filename))
+        question = {"id": id, "submission_time": datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "view_number": 0,
+                    "vote_number": 0, "title": title, "message": message, "image": f.filename}
+        data_handler.writer(question, "sample_data/question.csv", "a")
+    return redirect(f"/question/{id}")
+
+
+@app.route("/file_upload", methods=["GET", "POST"])
+def file_upload():
+    return render_template("file_upload.html")
+
+
+@app.route("/question/<question_id>/edit")
+def edit_question(question_id):
+    question = data_handler.reader("sample_data/question.csv")
+    return  render_template("edit.html", question = question, id = question_id)
+
+
+@app.route("/edit/<id>", methods=["GET", "POST"])
+def edit_quest(id):
+    title = request.form["title"]
+    message = request.form["message"]
+    question = {"id": id, "title": title, "message": message}
+    data_handler.edit(question, "sample_data/question.csv")
+    return redirect(f"/question/{id}")
+
+
+@app.route("/question/<question_id>/delete", methods=["GET", "POST"])
+def delete_question(question_id):
+    data_handler.delete(question_id)
+    return redirect("/")
+
+
+@app.route("/answer/<answer_id>/delete/<question_id>", methods=["GET", "POST"])
+def delete_answer(answer_id, question_id):
+    data_handler.delete_answer(answer_id)
+    return redirect(f"/question/{question_id}")
+
+
+@app.route("/question/<question_id>/vote_down", methods=["GET", "POST"])
+def vote_down(question_id):
+    question = data_handler.reader("sample_data/question.csv")
+    for row in question:
+        if row["id"] == question_id:
+            if int(row["vote_number"]) > 0:
+                row["vote_number"] = int(row["vote_number"]) - 1
+                data_handler.edit(row, "sample_data/question.csv")
+    return redirect("/sort?sort=vote_number&dir=down")
+
+
+@app.route("/question/<question_id>/vote_up", methods=["GET", "POST"])
+def vote_up(question_id):
+    question = data_handler.reader("sample_data/question.csv")
+    for row in question:
+        if row["id"] == question_id:
+            row["vote_number"] = int(row["vote_number"]) + 1
+            data_handler.edit(row, "sample_data/question.csv")
+    return redirect("/sort?sort=vote_number&dir=down")
+
+
+@app.route("/answer/<answer_id>/vote_down/<question_id>", methods=["GET", "POST"])
+def answer_vote_down(answer_id, question_id):
+    answer = data_handler.reader("sample_data/answer.csv")
+    for row in answer:
+        if row["id"] == answer_id:
+            if int(row["vote_number"]) > 0:
+                row["vote_number"] = int(row["vote_number"]) - 1
+                data_handler.edit(row, "sample_data/answer.csv")
+    return redirect(f"/question/{question_id}")
+
+
+@app.route("/answer/<answer_id>/vote_up/<question_id>", methods=["GET", "POST"])
+def answer_vote_up(answer_id, question_id):
+    answer = data_handler.reader("sample_data/answer.csv")
+    for row in answer:
+        if row["id"] == answer_id:
+            row["vote_number"] = int(row["vote_number"]) + 1
+            data_handler.edit(row, "sample_data/answer.csv")
+    return redirect(f"/question/{question_id}")
+
+
+@app.route("/sort", methods=["GET", "POST"])
+def sort():
+     sorting = request.query_string
+     sorting = sorting.decode()
+     print("query", sorting)
+     # x = request.form["sort"]  # if select sort
+     sorted = data_handler.sort(sorting)
+     return render_template("list.html", questions=sorted)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
