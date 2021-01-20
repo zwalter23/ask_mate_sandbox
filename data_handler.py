430510466525de969a614e116f9858dedbc84551
smarcell91 <71074300+smarcell91@users.noreply.github.com>
2021-01-20 16:49:57 +0100

    Add files via upload

--- data_handler.py
+++ data_handler.py
@@ -16,37 +16,38 @@ ALLOWED_EXTENSIONS = {'png', 'jpg'}
 @connect_database.connection_handler
 def writer(cursor: RealDictCursor, data, table):
     if table == 'question':
-        query = f"""INSERT INTO question (submission_time, view_number, vote_number, title, message, image)
-                       VALUES (%s, %s, %s, %s, %s, %s)"""
-        values = (data['submission_time'], data['view_number'], data['vote_number'], data['title'],
+        query = f"""INSERT INTO question (id, submission_time, view_number, vote_number, title, message, image)
+                       VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+        values = (data['id'], data['submission_time'], data['view_number'], data['vote_number'], data['title'],
                 data['message'], data['image'])
     elif table == 'answer':
-        query = f"""INSERT INTO answer (submission_time, vote_number, question_id, message, image)
-                       VALUES(%s, %s, %s, %s, %s)"""
-        values = (data['submission_time'], data['vote_number'], data['question_id'], data['message'], data['image'])
-    # elif table == 'comment':
-    #     if data['question_id'] == 'NULL':
-    #         query = f"""INSERT INTO comment
-    #                        VALUES(DEFAULT, NULL, %s, %s, %s, %s)"""
-    #         values = (data['answer_id'], data['message'], data['submission_time'],
-    #                   data['edited_count'])
-    #     else:
-    #         query = f"""INSERT INTO comment
-    #                        VALUES(DEFAULT, %s, NULL, %s, %s, %s)"""
-    #         values = (data['question_id'], data['message'], data['submission_time'],
-    #                   data['edited_count'])
+        query = f"""INSERT INTO answer (id, submission_time, vote_number, question_id, message, image)
+                       VALUES(%s, %s, %s, %s, %s, %s)"""
+        values = (data['id'], data['submission_time'], data['vote_number'], data['question_id'], data['message'], data['image'])
+    elif table == 'comment':
+        if data['question_id'] == 'NULL':
+            query = f"""INSERT INTO comment
+                           VALUES(%s, NULL, %s, %s, %s, %s)"""
+            values = (data['id'], data['answer_id'], data['message'], data['submission_time'],
+                      data['edited_count'])
+        else:
+            print(data)
+            query = f"""INSERT INTO comment
+                           VALUES(%s, %s, NULL, %s, %s, %s)"""
+            values = (data['id'], data['question_id'], data['message'], data['submission_time'],
+                      data['edited_count'])
     cursor.execute(query, values)
 
 
 @connect_database.connection_handler
 def reader(cursor: RealDictCursor, data):
-    cursor.execute(f"SELECT * FROM {data} ORDER BY id DESC")
+    cursor.execute(f"SELECT * FROM {data}")
     return cursor.fetchall()
 
 
 @connect_database.connection_handler
 def edit_question(cursor:RealDictCursor, data):
-    cursor.execute(f"""UPDATE question 
+    cursor.execute("""UPDATE question 
                 SET title = %s, message = %s
                 WHERE id = %s
     """, (data['title'], data['message'], data['id']))
@@ -58,6 +59,16 @@ def edit_answer(cursor:RealDictCursor, data):
                 SET message = '{data['message']}'
                 WHERE id = '{data['id']}'
         """)
+
+
+@connect_database.connection_handler
+def edit_comment(cursor:RealDictCursor, data):
+    cursor.execute(f"""UPDATE comment 
+                    SET message = '{data['message']}'
+                    WHERE id = '{data['id']}'
+            """)
+
+
 @connect_database.connection_handler
 def edit(cursor:RealDictCursor, id, modify):
     if modify =="downvote":
@@ -78,20 +89,6 @@ def edit(cursor:RealDictCursor, id, modify):
 
 
 @connect_database.connection_handler
-def edit_answer(cursor:RealDictCursor, id, modify):
-    if modify =="downvote":
-        cursor.execute(f""" UPDATE answer  
-                            SET vote_number = vote_number - 1
-                            WHERE id = '{id}'
-                        """)
-    elif modify == "upvote":
-        cursor.execute(f""" UPDATE answer  
-                            SET vote_number = vote_number + 1
-                            WHERE id = '{id}'
-                        """)
-
-
-@connect_database.connection_handler
 def delete_answer(cursor:RealDictCursor, answer_id):
     cursor.execute(f"SELECT image FROM answer WHERE id = {answer_id}")
     img = cursor.fetchall()
@@ -99,6 +96,7 @@ def delete_answer(cursor:RealDictCursor, answer_id):
     if img:
         if os.path.exists(f"static/img/answer/{img}"):
             os.remove(f"static/img/answer/{img}")
+    cursor.execute(f"DELETE FROM comment WHERE answer_id = {answer_id}")
     cursor.execute(f"DELETE FROM answer WHERE id = {answer_id}")
 
 
@@ -108,7 +106,7 @@ def delete(cursor:RealDictCursor, id):
     quest_img = cursor.fetchall()
     try:
         quest_img = [pic for pic in quest_img][0]['image']
-        print("DATACHECK:",quest_img)
+
         if os.path.exists(f"static/img/question/{quest_img}"):
             os.remove(f"static/img/question/{quest_img}")
     except:
@@ -116,20 +114,50 @@ def delete(cursor:RealDictCursor, id):
 
 
     cursor.execute(f"SELECT image FROM answer WHERE question_id = {id}")
-    answer_img = cursor.fetchall()
+    answer = cursor.fetchall()
     try:
-        answer_img = [pic for pic in answer_img][0]['image']
-        print("DATACHECK:", answer_img)
+        answer_img = [pic for pic in answer][0]['image']
         if os.path.exists(f"static/img/answer/{answer_img}"):
             os.remove(f"static/img/answer/{answer_img}")
     except:
         print("No answer image")
 
-    cursor.execute(f"DELETE FROM answer WHERE question_id = {id}")
-    cursor.execute(f"DELETE FROM question  WHERE id = {id}")
+    # cursor.execute(f"DELETE FROM comment WHERE question_id = {id} OR answer_id = {answ_id}")
+    cursor.execute("DELETE FROM answer WHERE question_id = %s", id)
+    cursor.execute("DELETE FROM question  WHERE id = %s", id)
+
+
+@connect_database.connection_handler
+def delete_comment(cursor: RealDictCursor, id):
+    cursor.execute("DELETE FROM comment WHERE id = %s", id)
 
 
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
+def id_generator(file):
+    id = 1
+    read = reader(file)
+    for row in read:
+        if row:
+            id = int(row["id"]) + 1
+    return id
+
+
+def sort(x):
+    d = re.split("=|&", x)[3]
+    y = re.split("=|&", x)[1]
+    print("sorter:",x, d)
+    questions = reader("question")
+    questions = list(questions)
+    try:
+        lst = sorted(questions, key = lambda i: int(i[y]))
+    except:
+        lst = sorted(questions, key=lambda i: i[y].lower())
+    if d == "down":
+        lst = lst[::-1]
+    for row in lst:
+        print(row)
+    return lst
